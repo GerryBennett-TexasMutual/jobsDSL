1f4fc5b48284d20ee7dcb22bed96e29abd063849
Gerald E. Bennett <GerryBennett@texasmutual.com>
2019-04-16 14:03:19 -0500

    Looking at all apps in shadow

---
+++ src/jobs/tmoApps/DividendsJobs.groovy
@@ -0,0 +1,80 @@
+folder('xray_demo_tmoApps') {
+		    displayName('XRay Demo')
+		    description('Folder for XRAY Demo Analysis of current txmutual artifacts')
+}
+
+job('xray_demo_tmoApps/XRay Demo - Dividends') {
+
+  	  properties {
+        githubProjectUrl('file:///opt/XRayDemo/tmoApps/Dividends')
+    }
+  
+    scm {
+    	git {
+          remote {
+	  	       url('file:///opt/XRayDemo/tmoApps/Dividends')
+          }
+          branch('**/master')
+          extensions {
+                    wipeOutWorkspace()
+          }
+        }
+    }
+  
+  wrappers {
+          wrappers {
+        // colorizeOutput()
+              
+   	      /**
+     * Adds timestamps to the console log.
+     */
+        timestamps()
+    }
+	credentialsBinding {
+				usernamePassword('TEMP_DEPLOY_ACCOUNT_USER', 'TEMP_DEPLOY_ACCOUNT_PASSWORD','9638e69f-2433-4b3e-8b6b-0115a3a33864')
+  									     }
+    artifactoryGenericConfigurator {
+      details {
+        artifactoryName(null)
+        artifactoryUrl(null)
+        deployReleaseRepository {
+          keyFromText('testing')
+          keyFromSelect(null)
+          dynamicMode(true)
+        }
+        deploySnapshotRepository(null)
+        resolveReleaseRepository(null)
+        resolveSnapshotRepository(null)
+        userPluginKey(null)
+        userPluginParams(null)
+      }
+      useSpecs(true)
+      uploadSpec{
+        spec('{"files": [{"pattern": "target/*.war","target": "txmutual-libs-dev-local"}]}')
+        filePath(null)
+      }
+      downloadSpec(null)
+      deployerDetails(null)
+      resolverDetails(null)
+      deployerCredentialsConfig(null)
+      resolverCredentialsConfig(null)
+      deployPattern('')
+      resolvePattern('')
+      matrixParams('')
+      deployBuildInfo(true)
+      includeEnvVars(true)
+      envVarsPatterns {
+        includePatterns(null)
+        excludePatterns('*password*,*secret*,*key*') 
+      }
+      discardOldBuilds(true)
+      discardBuildArtifacts(true)
+      multiConfProject(false)
+      artifactoryCombinationFilter('')
+      deploymentProperties(null)
+      asyncBuildRetention(false) 
+      customBuildName('')
+      overrideBuildName(false)
+    }
+  }
+}
